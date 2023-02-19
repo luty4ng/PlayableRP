d3dca74a370e424417c547686bb0128f5ab5de49
Luty <lutydata@gmail.com>
2023-02-19 17:20:07 +0800

    lighting

---
+++ Assets/PlayableRP/Runtime/PlayableRP.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayableRP : RenderPipeline {
+
+	ForwardRenderer renderer = new ForwardRenderer();
+
+	bool useDynamicBatching, useGPUInstancing;
+
+	ShadowSettings shadowSettings;
+
+	public PlayableRP (
+		bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher,
+		ShadowSettings shadowSettings
+	) {
+		this.shadowSettings = shadowSettings;
+		this.useDynamicBatching = useDynamicBatching;
+		this.useGPUInstancing = useGPUInstancing;
+		GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
+		GraphicsSettings.lightsUseLinearIntensity = true;
+	}
+
+	protected override void Render (
+		ScriptableRenderContext context, Camera[] cameras
+	) {
+		foreach (Camera camera in cameras) {
+			renderer.Render(
+				context, camera, useDynamicBatching, useGPUInstancing,
+				shadowSettings
+			);
+		}
+	}
+}
---
+++ Assets/PlayableRP/Runtime/PlayableRPAsset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Playable RP")]
+public class PlayableRPAsset : RenderPipelineAsset {
+
+	[SerializeField]
+	bool useDynamicBatching = true, useGPUInstancing = true, useSRPBatcher = true;
+
+	[SerializeField]
+	ShadowSettings shadows = default;
+
+	protected override RenderPipeline CreatePipeline () {
+		return new PlayableRP(
+			useDynamicBatching, useGPUInstancing, useSRPBatcher, shadows
+		);
+	}
+}
